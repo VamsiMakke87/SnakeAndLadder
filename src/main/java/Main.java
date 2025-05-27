5ab4b3be4be556e8d938146f23635682ac0e70f3
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-05-27 12:33:23 -0400

    Implemented core game logic to run the Snake and Ladder gameplay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+import model.Board;
+import model.Player;
 import model.config.GameConfig;
 import model.dice.DiceType;
 import model.obstacle.Obstacle;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -12,11 +13,19 @@ public class Main {
         try {
 
             Scanner sc = new Scanner(System.in);
+            Deque<Player> playerDeque=new ArrayDeque<>();
             System.out.println("Enter number of players:");
-            int numberOfPlayers = sc.nextInt();
+            int numberOfPlayers = Integer.parseInt(sc.nextLine());
+            for (int i=1;i<=numberOfPlayers;i++){
+                System.out.println("Enter player "+i+" name:");
+                String playerName= sc.nextLine();
+                playerDeque.add(new Player(playerName));
+            }
             System.out.println("Enter board size:");
-            int boardSize = sc.nextInt();
+            int boardSize = Integer.parseInt(sc.nextLine());
             DiceType diceType;
+
+            System.out.println(playerDeque);
 
             while (true) {
                 try {
@@ -32,7 +41,7 @@ public class Main {
             int numberOfDice;
             while (true) {
                 System.out.println("Enter number of dice:");
-                numberOfDice = sc.nextInt();
+                numberOfDice = Integer.parseInt(sc.next());
                 if (numberOfDice > boardSize) {
                     System.out.println("Number of dice cannot be more than board size(" + boardSize + ")");
                 } else {
@@ -40,12 +49,33 @@ public class Main {
                 }
             }
             GameConfig gameConfig = new GameConfig(numberOfDice, boardSize, diceType);
+            Board.intialize(gameConfig);
+            Board board=Board.getInstance();
+            List<Player> winners=new ArrayList<>();
 
 
+            while (true){
 
+                Player currPlayer= playerDeque.removeFirst();
+                int previousPosition=currPlayer.getPositon();
+                System.out.println(currPlayer.getName()+"'s turn");
+                board.rollDice(currPlayer);
+                if(currPlayer.getPositon()==boardSize){
+                    System.out.println("______________________________________________");
+                    System.out.println(currPlayer.getName()+" reached destination");
+                    System.out.println("______________________________________________");
+                    winners.add(currPlayer);
+                    if(playerDeque.size()==1)break;
+                }else{
+                    System.out.println(currPlayer.getName()+" move from "+previousPosition+" to "+currPlayer.getPositon());
+                    playerDeque.addLast(currPlayer);
+                }
+            }
 
-
-
+            for(int i=0;i<winners.size();i++){
+                System.out.println("Winner "+(i+1)+":"+winners.get(i).getName());
+            }
+            System.out.println(playerDeque.removeFirst().getName()+" lost");
         } catch (Exception e) {
             System.out.println("Unknown error occured! Please restart the app");
         }
--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,10 +1,13 @@
 package model;
 
 import model.config.GameConfig;
+import model.dice.Dice;
+import model.exception.InvalidDiceTypeException;
 import model.obstacle.Obstacle;
 import model.obstacle.ObstacleType;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -13,59 +16,96 @@ public class Board {
     private static GameConfig gameConfig;
 
 
-    private static final Map<Integer, Obstacle> obstacleMap= new HashMap<>();;
+    private static final Map<Integer, Obstacle> obstacleMap = new HashMap<>();
+    ;
     private static Board instance;
 
-    private static Random random=new Random();
+    private static Random random = new Random();
 
     private Board(GameConfig gameConfig) {
         this.gameConfig = gameConfig;
-        obstacleMap.put(0,null);
+        obstacleMap.put(0, null);
     }
 
-    public static synchronized void intialize(GameConfig gameConfig){
-        if(instance==null){
-            instance=new Board(gameConfig);
+    public static synchronized void intialize(GameConfig gameConfig) {
+        if (instance == null) {
+            instance = new Board(gameConfig);
             generateLadders();
             generateSnakes();
         }
 
     }
 
-    private static void generateLadders(){
-        int numberOfLadders= random.nextInt(6)+1; // randomly generating n ladders- maximum 6 ladders
+    private static void generateLadders() {
+        int numberOfLadders = random.nextInt(6) + 1; // randomly generating n ladders- maximum 6 ladders
 
-        for(int i=0;i<numberOfLadders;i++){
-            int start=0;
-            while(obstacleMap.containsKey(start)){
-                start=random.nextInt(gameConfig.getBoardSize());
+        for (int i = 0; i < numberOfLadders; i++) {
+            int start = 0;
+            while (obstacleMap.containsKey(start)) {
+                start = random.nextInt(gameConfig.getBoardSize());
             }
-            int end=random.nextInt(start+1,gameConfig.getBoardSize());
+            int end = random.nextInt(start + 1, gameConfig.getBoardSize());
 
-            obstacleMap.put(start,new Obstacle(start,end, ObstacleType.LADDER));
+            obstacleMap.put(start, new Obstacle(start, end, ObstacleType.LADDER));
         }
     }
 
-    private static void generateSnakes(){
-        int numberOfSnakes= random.nextInt(6)+1; // randomly generating n snakes- maximum 6 snakes
-        for(int i=0;i<numberOfSnakes;i++){
-            int start=0;
-            while(obstacleMap.containsKey(start)){
-                start=random.nextInt(gameConfig.getBoardSize());
+    private static void generateSnakes() {
+        int numberOfSnakes = random.nextInt(6) + 1; // randomly generating n snakes- maximum 6 snakes
+        for (int i = 0; i < numberOfSnakes; i++) {
+            int start = 0;
+            while (obstacleMap.containsKey(start)) {
+                start = random.nextInt(gameConfig.getBoardSize());
             }
-            int end=random.nextInt(1,start);
+            int end = random.nextInt(1, start);
 
-            obstacleMap.put(start,new Obstacle(start,end, ObstacleType.SNAKE));
+            obstacleMap.put(start, new Obstacle(start, end, ObstacleType.SNAKE));
         }
     }
 
-    public static Board getInstance(){
-        if(instance==null){
+    public static Board getInstance() {
+        if (instance == null) {
             throw new IllegalStateException("Initilaize the board first");
         }
         return instance;
     }
 
-    
+    public void rollDice(Player player) throws InvalidDiceTypeException {
+        List<Dice> dices = gameConfig.getDices();
+        int numberOfStepsToMove = 0;
+        for (int i = 0; i < dices.size(); i++) {
+            int diceFace = dices.get(i).roll();
+            System.out.println("Dice 1: " + diceFace);
+            numberOfStepsToMove += diceFace;
+        }
+
+        System.out.println("Number of steps to move:" + numberOfStepsToMove);
+
+        moveSteps(player, numberOfStepsToMove);
+
+    }
+
+    private void moveSteps(Player player, int numberOfStepsToMove) {
+
+        int currPosition = player.getPositon();
+        int nextPosition = currPosition + numberOfStepsToMove;
+        if (nextPosition > gameConfig.getBoardSize()) {
+            nextPosition=currPosition;
+            System.out.println("Can only move " + (gameConfig.getBoardSize() - currPosition) + " places");
+        } else if(nextPosition < gameConfig.getBoardSize()) {
+            if (obstacleMap.containsKey(nextPosition)) {
+                Obstacle obstacle = obstacleMap.get(nextPosition);
+                if (obstacle.getObstacleType() == ObstacleType.LADDER) {
+                    System.out.println(player.getName() + " found a ladder!");
+                } else {
+                    System.out.println(player.getName() + " bitten by a snake");
+                }
+                nextPosition = obstacle.getEnd();
+            }
+        }
+
+        player.setPositon(nextPosition);
+
+    }
 
 }
