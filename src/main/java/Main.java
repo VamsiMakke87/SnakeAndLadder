9bcfc10270a2e9903972bfc62b54cbfe0708e740
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-05-27 12:58:53 -0400

    Added validation in Main to enforce minimum 2 players before starting the game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,12 +13,20 @@ public class Main {
         try {
 
             Scanner sc = new Scanner(System.in);
-            Deque<Player> playerDeque=new ArrayDeque<>();
+            Deque<Player> playerDeque = new ArrayDeque<>();
             System.out.println("Enter number of players:");
-            int numberOfPlayers = Integer.parseInt(sc.nextLine());
-            for (int i=1;i<=numberOfPlayers;i++){
-                System.out.println("Enter player "+i+" name:");
-                String playerName= sc.nextLine();
+            int numberOfPlayers = 0;
+            while (true) {
+                numberOfPlayers = Integer.parseInt(sc.nextLine());
+                if (numberOfPlayers < 1) {
+                    System.out.println("Minimum 2 players required to start the game!");
+                } else {
+                    break;
+                }
+            }
+            for (int i = 1; i <= numberOfPlayers; i++) {
+                System.out.println("Enter player " + i + " name:");
+                String playerName = sc.nextLine();
                 playerDeque.add(new Player(playerName));
             }
             System.out.println("Enter board size:");
@@ -50,32 +58,32 @@ public class Main {
             }
             GameConfig gameConfig = new GameConfig(numberOfDice, boardSize, diceType);
             Board.intialize(gameConfig);
-            Board board=Board.getInstance();
-            List<Player> winners=new ArrayList<>();
+            Board board = Board.getInstance();
+            List<Player> winners = new ArrayList<>();
 
 
-            while (true){
+            while (true) {
 
-                Player currPlayer= playerDeque.removeFirst();
-                int previousPosition=currPlayer.getPositon();
-                System.out.println(currPlayer.getName()+"'s turn");
+                Player currPlayer = playerDeque.removeFirst();
+                int previousPosition = currPlayer.getPositon();
+                System.out.println(currPlayer.getName() + "'s turn");
                 board.rollDice(currPlayer);
-                if(currPlayer.getPositon()==boardSize){
+                if (currPlayer.getPositon() == boardSize) {
                     System.out.println("______________________________________________");
-                    System.out.println(currPlayer.getName()+" reached destination");
+                    System.out.println(currPlayer.getName() + " reached destination");
                     System.out.println("______________________________________________");
                     winners.add(currPlayer);
-                    if(playerDeque.size()==1)break;
-                }else{
-                    System.out.println(currPlayer.getName()+" move from "+previousPosition+" to "+currPlayer.getPositon());
+                    if (playerDeque.size() == 1) break;
+                } else {
+                    System.out.println(currPlayer.getName() + " move from " + previousPosition + " to " + currPlayer.getPositon());
                     playerDeque.addLast(currPlayer);
                 }
             }
 
-            for(int i=0;i<winners.size();i++){
-                System.out.println("Winner "+(i+1)+":"+winners.get(i).getName());
+            for (int i = 0; i < winners.size(); i++) {
+                System.out.println("Winner " + (i + 1) + ":" + winners.get(i).getName());
             }
-            System.out.println(playerDeque.removeFirst().getName()+" lost");
+            System.out.println(playerDeque.removeFirst().getName() + " lost");
         } catch (Exception e) {
             System.out.println("Unknown error occured! Please restart the app");
         }
